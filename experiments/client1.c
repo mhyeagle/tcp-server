0f9f4b797736fd926a636e4c7f8efadfa467178c
mhyeagle <tianyashuibin@163.com>
2017-03-03 19:16:11 +0800

    add experiments/server1

---
+++ experiments/client1.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+
+void error(char *msg) {
+    perror(msg);
+    exit(0);
+}
+
+int main(int argc, char *argv[]) {
+    int sockfd, portno, n;
+
+
+    return 0;
+}
