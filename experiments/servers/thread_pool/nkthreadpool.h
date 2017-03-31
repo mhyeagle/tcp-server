92e21f9ad3a5847f7b9291ecf9c68a1abfac0700
luna <tianyashuibin@163.com>
2017-03-31 22:17:11 +0800

    thread-pool update

--- experiments/servers/thread_pool/nkthreadpool.h
+++ experiments/servers/thread_pool/nkthreadpool.h
@@ -1,29 +1,46 @@
 #ifndef NK_THREADPOOL_H
 #define NK_THREADPOOL_H
 
+#include <memory>
+#include <pthread.h>
+
 #include "nkqueue.h"
 #include "nktask.h"
+
 
 class NKThreadpool {
 public:
     NKThreadpool(): threads_num__(2) {
         queue__->reserve(100);
-        workers->new pthread_t[2];
-        for(int i = 0; i < 2; ++i) {
-            
-        }
+		create_workers(2);
     }
 
     NKThreadpool(int thread_num, int queue_size): threads_num__(thread_num) {
         queue__->reserve(queue_size);
+		create_workers(thread_num);
     }
 
+	static void* worker_func(void* arg);
     int run(NKTask* task);
     int run(std::vector<NKTask*>& tasks);
 
 private:
+	int create_workers(int size) {
+		workers__ = new pthread_t[size];
+		int ret = 0;
+		for(int i = 0; i < size; ++i) {
+			ret = pthread_create(workers+i, NULL, worker_func, NULL);
+			if (ret) {
+				std::cout << "pthread_create failed." << std::endl;
+				break;
+			}
+		}
+		return ret;
+	}
+
+private:
     int threads_num__;
-    std::shared_ptr<> workers__;
+    pthread_t* workers__;
     std::shared_ptr<NKQueue<NKTask> > tasks__;
 };
 
